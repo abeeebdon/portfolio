d92c45b95a1d042b895fcc7517e5cd3ce795ae11
Abeeb <you@example.com>
2023-09-04 00:05:54 +0100

    Making it responsive

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
 
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.addEventListener('click', click)
+function click() {
+  menuIcon.classList.toggle('bx-x')
+  navbar.classList.toggle('active')
+  console.log(100)
+}
 window.onscroll = () => {
   sections.forEach((sect) => {
     let top = window.scrollY
@@ -19,4 +28,8 @@ window.onscroll = () => {
   //   sticky navbar
   let header = document.querySelector('header')
   header.classList.toggle('sticky', window.scrollY > 100)
+
+  menuIcon.classList.remove('bx-x')
+  menuIcon.classList.remove('active')
 }
+// toggle icon navbar
